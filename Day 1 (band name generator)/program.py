7e259f36c9f071a5cce3646e9bd3369d70b75c2f
aditya <aditya.pradeepg2000@gmail.com>
2024-03-26 16:23:55 +0530

    day1

---
+++ Day 1 (band name generator)/program.py
@@ -0,0 +1,4 @@
+print("\nWelcome to the band name generator\n")
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("The name of your band is {} {}".format(city, pet_name))
